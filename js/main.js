05606ab8e8cc2cc5ae1e95047b3e232f26db01ec
M4rqu1705 <m4rc05.dev@gmail.com>
2019-10-27 00:29:19 -0400

    Started styling css. Every input cell was tested and debugged. It should theoretically work. However, CSS,is being problematic

--- js/main.js
+++ js/main.js
@@ -2,9 +2,8 @@
 var Clue,
     Answer,
     Value,
-    //AirDate = [ new Date("January 1, 1985"), new Date("August 1, 1985") ]
-    AirDate = [0,0],
-    Category="1";
+    AirDate = [ new Date("January 1, 1950"), new Date() ],
+    Category;
 var categories;
 
 // HELPER FUNCTIONS --- HELPER FUNCTIONS --- HELPER FUNCTIONS --- HELPER FUNCTIONS
@@ -19,8 +18,9 @@ function sanitize(str) {
         '"': '&quot;',
         "'": '&#x27;',
         "/": '&#x2F;',
+        "?": '&#63;'
     };
-    const reg = /[&<>"'/]/ig;
+    const reg = /[&<>"'?/]/ig;
     var output = str.replace(reg, (match)=>(map[match]));
     return(output);
 }
@@ -33,118 +33,118 @@ function defined(data){
 // Show loading spinner
 function loadingSpinner(state){
     if(state){
-        $('#loadingIcon').show();
+        document.getElementById("loadingIcon").style.display = "block";
     }else{
-        $('#loadingIcon').hide();
+        document.getElementById("loadingIcon").style.display = "none";
     }
 
 }
 
 function getURL(){
-    // Sanitize all data before anything
-    if(defined(Category)) Category = String(sanitize(Category));
-    if(defined(Value)) Value = Number(sanitize(Value));
-    if(defined(AirDate)) AirDate[0] = Date(sanitize(AirDate[0])), AirDate[1] = Date(sanitize(AirDate[1]));
+    // https://cors-anywhere.herokuapp.com/
+    url=['http://jservice.io/api/clues/?'];
 
-    url=['https://cors-anywhere.herokuapp.com/http://jservice.io/api/clues/?'];
-
-    if(defined(Category)){
-        // Get categories if not retrieved yet
-        if(!defined(categories)){
-            $.ajax({
-                // url:        "categories.json",
-                url:        "https://M4rqu1705.github.io/Jeopardy-API-UI/categories.json",
-                cache:      false,
-                dataType:   "json",
-                success:    function(data){
-                    categories = data;
-                    autocomplete(document.getElementById("categoryInput"), categories);
-                },
-                error:      function(err){
-                    console.log(err);
-                }
-            });
-        }
-
-        if(defined(categories)){
-            matches = [];
-            keys = Object.keys(categories);
-            for(let c = 0; c<keys.length; c++){
-                if(RegExp(Category, "i").test(keys[c])){
-                    matches.push(keys[c]);
-                }
+    // Get categories if not retrieved yet
+    if(!defined(categories)){
+        $.ajax({
+            // url:        "categories.json",
+            url:        "https://M4rqu1705.github.io/Jeopardy-API-UI/categories.json",
+            cache:      false,
+            dataType:   "json",
+            success:    function(data){
+                categories = data;
+                autocomplete(document.getElementById("categoryInput"), categories);
+            },
+            error:      function(err){
+                console.log(err);
             }
-            url.push("category=".concat(categories[matches[0]]));
-        }
+        });
     }
 
-    if([100, 200, 300, 400, 500, 600].includes(Value)){
+    if(defined(Category) && defined(categories)){
+        matches = [];
+        keys = Object.keys(categories);
+        url.push("category=".concat(categories[Category]));
+    }
+
+    if(defined(Value) && [100, 200, 300, 400, 500, 600].includes(Value)){
         url.push("value=".concat(Value));
     }
 
-    if(typeof AirDate[0].getMonth === 'function' && AirDate[0] <= new Date()){
-        day = AirDate[0].toISOString();
+    if(defined(AirDate) && typeof AirDate[0].getMonth === 'function' && AirDate[0] <= new Date()){
+        day = AirDate[0].toISOString().slice(0,10);
         url.push("min_date=".concat(day));
     }
-    if(typeof AirDate[1].getMonth === 'function'){
-        day = AirDate[1].toISOString();
+    if(defined(AirDate) && typeof AirDate[1].getMonth === 'function'){
+        day = AirDate[1].toISOString().slice(0,10);
         url.push("max_date=".concat(day));
     }
 
     if(url.length == 1){
-        url = 'https://cors-anywhere.herokuapp.com/http://jservice.io/api/random/?count=10';
+        // https://cors-anywhere.herokuapp.com/
+        url = 'http://jservice.io/api/random/?count=10';
     }else{
-        url = url.join("&");
+        url = url[0] + url.slice(1).join("&");
     }
 
     return(url);
 }
 
 function updateContent(){
-
     var timer = setTimeout(function() {
         $.ajax({
             url:        getURL(),
             dataType:   "json",
             success:    function(data){
+
+                let searchResultsD = document.getElementById('searchResults');
+
                 // Empty search results before adding content
-                $('#searchResults').empty();
+                while(searchResultsD.firstChild){
+                    searchResults.removeChild(searchResults.firstChild);
+                }
+
                 for(let c = 0; c<data.length; c++){
+                    // Extract data of interest
                     let answer =    String(data[c].answer),
                         question =  String(data[c].question),
                         value =     Number(data[c].value),
                         airdate =   new Date(data[c].airdate),
                         category =  String(data[c].category.title);
 
-                    // Structures to help normalize data 
+
+                    // Structures to help normalize data
                     month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
                     function capitalize(string) { return string.charAt(0).toUpperCase() + string.slice(1); }
 
                     airdate = " ".concat(month[new Number(airdate.getMonth())], " " , new String(airdate.getDate()), ", ", new String(airdate.getFullYear()));
 
-                    // const TEXT_WIDTH = 40
-                    // if(question.length > TEXT_WIDTH){
-                    // question = question.slice(0, TEXT_WIDTH-3).concat("...")
-                    // }
+                    const TEXT_WIDTH = 50;
+                    if(question.length > TEXT_WIDTH){
+                        question = question.slice(0, TEXT_WIDTH-3).concat("...")
+                    }
 
+                    // Entry container
+                    entryD = document.createElement('div');
+                    entryD.classList.add('entry');
 
-                    entryD = $('<div><div>').attr({
-                        "class":"entry"
-                    });
-                    questionP = $('<p></p>').attr({
-                        "class":"question"
-                    });
-                    detailsP = $('<p></p>').attr({
-                        "class":"details"
-                    });
+                    // Question
+                    questionP = document.createElement('p');
+                    questionP.classList.add('question');
+                    if(question.length > 1)
+                        questionP.textContent = question;
+                    else
+                        break;
 
-                    questionP.text(question);
-                    detailsP.text([
-                        capitalize(category),
-                        airdate,
-                        value].join(" • "));
+                    // Details
+                    detailsP = document.createElement('p');
+                    detailsP.classList.add('details');
+                    detailsP.textContent = [capitalize(category), airdate, value].join(" • ");
 
-                    entryD.append(questionP, detailsP)
+                    // Other data
+
+                    entryD.appendChild(questionP);
+                    entryD.appendChild(detailsP);
 
                     // #############################################################
                     // ###################### ON CLICK #############################
@@ -163,113 +163,240 @@ function updateContent(){
 
     }, 1);
 }
+
+function triggerSearch(){
+    loadingSpinner(true);
+    updateContent();
+}
+
+// ======================================
+// ======== Category Input field ========
+// ======================================
+
+categoryInput = document.getElementById("categoryInput");
+
 function autocomplete(field, possibleValues) {
     // Categories dictionary is passed in, but instead we need the keys
     possibleValues = Object.keys(possibleValues);
 
     var currentFocus;
 
-    field.addEventListener("input", function(e) {
-        var a, b, i, val = this.value;
+    field.addEventListener('input', function(e) {
+        var matchesD, suggestionD, i, value = this.value;
         // Close any already open lists of autocompleted values
         closeAllLists();
 
         // Do not waste time on empty fields
-        if (!val) return(false);
-        // Do not waste time on other than characters
-        if (!/[A-Za-z0-9_'"]/.test(val)){
-            console.log("Exited auto completion");
+        if (!value){
+            Categories = undefined;
+            return(false);
+        };
+        // Do not waste time on strange characters
+        if (!/[A-Za-z0-9_'"]/.test(value)){
             return(false);
         }
 
         currentFocus = -1;
 
         // Container for matches ...
-        a = document.createElement("DIV");
-        a.setAttribute("id", this.id + "autocomplete-list");
-        a.setAttribute("class", "autocomplete-items");
+        matchesD = document.createElement('div');
+        matchesD.setAttribute('id', this.id + 'autocomplete-list');
+        matchesD.setAttribute('class', 'autocomplete-items');
+        matchesD.style.width = this.style.width;
+        matchesD.style.position = "fixed";
+        matchesD.style.top = $(this).offset().top + this.style.height;
+        matchesD.offsetLeft = $(this).offset().left;
+        matchesD.style.zIndex = "10";
 
         // Append the DIV element as a child of the autocomplete container:
-        this.parentNode.appendChild(a);
+        this.parentNode.appendChild(matchesD);
+        // document.getElementsByTagName("body")[0].appendChild(matchesD);
 
         for (i = 0; i < possibleValues.length; i++) {
-            // Search criteria: first characters in common 
-            if (possibleValues[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+            // Search criteria: first characters in common
+            if (possibleValues[i].substr(0, value.length).toUpperCase() == value.toUpperCase()) {
                 match = possibleValues[i]
-                b = document.createElement("DIV");
+
+                suggestionD = document.createElement('div');
+                
+
                 // Make the matching letters bold
-                b.innerHTML = "<strong>" + match.substr(0, val.length) + "</strong>";
-                b.innerHTML += match.substr(val.length);
+                let content = '<strong>' + match.substr(0, value.length) + '</strong>';
+                content += match.substr(value.length);
 
                 // Insert a input field that will hold the current array item's value:
-                b.innerHTML += "<input type='hidden' value='" + match + "'>";
+                content += "<input type='hidden' value='" + sanitize(match) + "'>";
 
-                // End list once user selects this match
-                b.addEventListener("click", function(e) {
-                    field.value = this.getElementsByTagName("input")[0].value;
+                suggestionD.innerHTML = content;
+
+                suggestionD.addEventListener('click', function(e) {
+                    field.value = this.getElementsByTagName('input')[0].value;
                     Category = field.value;
                     closeAllLists();
-                    loadingSpinner(true);
-                    updateContent();
-
-
+                    triggerSearch();
                 });
-                a.appendChild(b);
+                matchesD.appendChild(suggestionD);
+
+                // Keep the list within 10 elements
+                if(matchesD.childNodes.length >= 10){
+                    break;
+                }
             }
         }
     });
 
-    /*execute a function presses a key on the keyboard:*/
-    field.addEventListener("keydown", function(e) {
-        var x = document.getElementById(this.id + "autocomplete-list");
-        if (x) x = x.getElementsByTagName("div");
-        if (e.keyCode == 40) { // DOWN Arrow
+    field.addEventListener('keydown', function(e) {
+        let list = document.getElementById(this.id + 'autocomplete-list');
+
+        if(list)
+            list = list.getElementsByTagName('div');
+
+        if(e.keyCode == 40) {          // Down Arrow
             currentFocus++;
-            addActive(x);
-        } else if (e.keyCode == 38) { //UP Arrow
+            addActive(list);
+        }else if (e.keyCode == 38) {   //Up Arrow
             currentFocus--;
-            addActive(x);
-        } else if (e.keyCode == 13) { // ENTER key
+            addActive(list);
+        }else if (e.keyCode == 13) {   // Enter key
             e.preventDefault();
-            if (currentFocus > -1) {
-                /*and simulate a click on the "active" item:*/
-                if (x){
-                    x[currentFocus].click();
+            if(defined(list)){
+                // Search for currently focused entry 
+                if(currentFocus > -1) {
+                    list[currentFocus].click();
+                }else{
+                    list[0].click();
                 }
             }
 
         }
     });
-    function addActive(x) {
-        /*a function to classify an item as "active":*/
-        if (!x) return false;
-        /*start by removing the "active" class on all items:*/
-        removeActive(x);
-        if (currentFocus >= x.length) currentFocus = 0;
-        if (currentFocus < 0) currentFocus = (x.length - 1);
-        /*add class "autocomplete-active":*/
-        x[currentFocus].classList.add("autocomplete-active");
+
+    function addActive(items) {
+        // Classify items as "active":
+        if (!items) return false;
+
+        // Start by removing the "active" class on all items
+        removeActive(items);
+
+        if (currentFocus >= items.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = (items.length - 1);
+
+        // Add class "autocomplete-active":
+        items[currentFocus].classList.add('autocomplete-active');
     }
-    function removeActive(x) {
-        /*a function to remove the "active" class from all autocomplete items:*/
-        for (var i = 0; i < x.length; i++) {
-            x[i].classList.remove("autocomplete-active");
+
+    function removeActive(items) {
+        // Remove the "active" class from all autocomplete items:
+        for (var i = 0; i < items.length; i++) {
+            items[i].classList.remove('autocomplete-active');
         }
     }
-    function closeAllLists(elmnt) {
-        /*close all autocomplete lists in the document,
-except the one passed as an argument:*/
-        var x = document.getElementsByClassName("autocomplete-items");
-        for (var i = 0; i < x.length; i++) {
-            if (elmnt != x[i] && elmnt != field) {
-                x[i].parentNode.removeChild(x[i]);
+
+    function closeAllLists(element) {
+        // Close all autocomplete lists in the document, except the one passed as an argument:
+        let items = document.getElementsByClassName('autocomplete-items');
+        for (let i = 0; i < items.length; i++) {
+            if (element != items[i] && element != field) {
+                items[i].parentNode.removeChild(items[i]);
             }
         }
     }
-    /*execute a function when someone clicks in the document:*/
-    document.addEventListener("click", function (e) {
+
+    document.addEventListener('click', function (e) {
     });
 }
 
 
+// ===================================
+// ======== Value Input field ========
+// ===================================
 
+valueInput = document.getElementById('valueInput');
+
+// Automatically delete contents on focus
+valueInput.addEventListener('focus', function(e) {
+    valueInput.value = '';
+    Value = undefined;
+});
+
+valueInput.addEventListener('keydown', function(e) {
+    // If backspace or delete pressed
+    if(e.keyCode == 8 || e.keyCode == 46){
+        valueInput.value = '';
+        Value = undefined;
+    }
+});
+
+// Put restraints to the user input
+valueInput.addEventListener('input', function(e) {
+    Value = valueInput.value;
+    if(Value < 100 && (1<=Value && Value <= 6)){
+        Value = Math.round(Value)*100;
+    }else if(0<Value && Value<1){
+        Value = 100;
+    }else if(6<Value && Value<100){
+        Value = 600;
+    }else if(Value > 600){
+        Value = 600;
+    }else{
+        Value = undefined;
+    }
+    valueInput.value = Value;
+});
+
+
+// ==================================
+// ======== Date Input field ========
+// ==================================
+
+startDateInput = document.getElementById('startDateInput');
+endDateInput = document.getElementById('endDateInput');
+
+// Set default dates
+startDateInput.value = new Date('January 1, 1950').toISOString().slice(0,10);
+endDateInput.value = new Date().toISOString().slice(0,10);
+
+startDateInput.addEventListener('input', function(e){
+    // Make sure startDate is always less than or equal to endDate 
+    if(new Date(startDateInput.value).getTime() > new Date(endDateInput.value).getTime()){
+        startDateInput.value = new Date(endDateInput.value).toISOString().slice(0,10);
+    }
+
+    if(String(startDateInput.value) == ""){
+        AirDate[0] = undefined;
+    }else{
+        AirDate[0] = new Date(startDateInput.value);
+    }
+});
+
+endDateInput.addEventListener('input', function(e){
+    // Make sure startDate is always less than or equal to endDate 
+    if(new Date(startDateInput.value).getTime() > new Date(endDateInput.value).getTime()){
+        endDateInput.value = new Date(startDateInput.value).toISOString().slice(0,10);
+    }
+
+    AirDate[1] = new Date(endDateInput.value);
+
+});
+
+searchButton = document.getElementById('searchButton');
+
+searchButton.addEventListener('click', function(e){
+    triggerSearch();
+});
+
+// =========================
+// ======== Nav Bar ========
+// =========================
+
+document.addEventListener('keydown', function(e){
+    if(e.keyCode == 13)
+        triggerSearch();
+});
+
+
+// ==========================
+// ======== Menu Bar ========
+// ==========================
+
+menuBar = document.getElementById('menuSection');
